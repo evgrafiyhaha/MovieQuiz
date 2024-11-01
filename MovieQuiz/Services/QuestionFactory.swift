ea8029de67b6300f97e0bfefd6c640d4f4e8ec82
artemiytolkisevskiy <evgrafiyha@gmail.com>
2024-11-01 23:29:49 +0300

    sprint_05 task completed

---
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+final class QuestionFactory: QuestionFactoryProtocol {
+    private var questions: [QuizQuestion] = [
+        QuizQuestion(
+            image: "The Godfather",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Dark Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Kill Bill",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Avengers",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Deadpool",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "The Green Knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+        QuizQuestion(
+            image: "Old",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "The Ice Age Adventures of Buck Wild",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Tesla",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false),
+        QuizQuestion(
+            image: "Vivarium",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: false)
+    ]
+
+    private var currentQuestionIndex: Int = .zero
+    weak private var delegate: QuestionFactoryDelegate?
+
+    func setup(delegate: QuestionFactoryDelegate) {
+        self.delegate = delegate
+        self.questions.shuffle()
+    }
+
+    func requestNextQuestion() {
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = .zero
+            questions.shuffle()
+        }
+
+        let question = questions[currentQuestionIndex]
+        currentQuestionIndex += 1
+        delegate?.didReceiveNextQuestion(question: question)
+    }
+}
+
